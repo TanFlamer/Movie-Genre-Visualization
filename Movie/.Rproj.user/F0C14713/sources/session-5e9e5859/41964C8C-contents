85d181c9d9ef35864a03a8c0c1468340bbded11a
TanFlamer <zxtan01@outlook.com>
2023-03-21 22:52:12 +0800

    R Project for Movie Dataset

---
+++ Movie/.Rproj.user/F0C14713/sources/session-5e9e5859/41964C8C-contents
@@ -0,0 +1,5 @@
+# sum(grepl("Adventure", action$genre, fixed = TRUE))
+
+# rm(list = ls())
+
+# cat("\014")
---
+++ Movie/coursework.R
@@ -0,0 +1,100 @@
+# Import library
+library(stringr)
+library(dplyr)
+
+# Zip file name
+zip_file <- "archive.zip"
+
+# Load all csv files
+csv_files <- unzip(zip_file, list = TRUE)$Name
+
+# Create empty data frame
+Movies <- data.frame()
+
+# Genre list
+GenreList <- c()
+
+# Loop through movie list
+for (csv in csv_files) {
+  # Load in csv file
+  temp <- read.csv(unzip(zip_file, csv))
+  
+  # Drop unused columns
+  temp <- subset(temp, select = -c(movie_id, description, director_id, star_id, gross.in...))
+  
+  # Extract genre name
+  genreName <- sub('.csv', '', csv)
+  
+  # Handle sci-fi case
+  genreName <- gsub("scifi", "sci-fi", genreName)
+  
+  # Capitalize first letter
+  genreName <- str_to_title(genreName)
+  
+  # Handle special case
+  genreName <- gsub("(-[a-z])", "\\U\\1", genreName, perl=TRUE)
+  
+  # Add to genre list
+  GenreList <- c(GenreList, genreName)
+  
+  # Add movies to data frame
+  Movies <- rbind(Movies, temp)
+  
+  # Remove csv file
+  file.remove(csv)
+}
+
+# Extract number from string
+Movies$runtime <- sub(' min', '', Movies$runtime)
+
+# Convert string to integer
+Movies$runtime <- strtoi(Movies$runtime)
+
+# Convert year to integer
+Movies$year <- strtoi(Movies$year)
+
+# Remove unmade movies
+Movies <- Movies[!is.na(Movies$year) & Movies$year <= 2023, ]
+
+# Get genre list
+genreList <- strsplit(Movies$genre, ", ")
+
+# Pad genre list with NA
+genreList <- lapply(genreList, "length<-", 3)
+
+# Separate genre list
+for (x in 1:3){
+  # Get column name
+  columnName <- paste("genre", x, sep = "")
+  
+  # Append new column
+  Movies[,columnName] <- sapply(genreList, "[[", x)
+}
+
+# Drop genre column
+Movies <- subset(Movies, select = -c(genre))
+
+# Remove duplicates
+Movies <- Movies %>% distinct()
+
+# Create genre combinations
+for (x in 1:3){
+  # Get column name
+  columnName <- paste("Genre", x, sep = "")
+  
+  # Generate genre combination
+  assign(columnName, combn(GenreList, x, simplify = FALSE))
+}
+
+# Add All movies option
+GenreList <- c("All", GenreList)
+
+# Remove unused variables
+rm(x)
+rm(csv)
+rm(temp)
+rm(zip_file)
+rm(csv_files)
+rm(genreList)
+rm(genreName)
+rm(columnName)
